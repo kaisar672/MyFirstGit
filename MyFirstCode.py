52571a3df7fd46b93f07f64d50a4f13bf7bf8864
kaisar672 <m.kaiserlone@gmail.com>
2024-02-16 22:07:45 +0530

    last commit

---
+++ MyFirstCode.py
@@ -0,0 +1,11 @@
+print("This is python program");
+fruits=["Banana","Apple","Orange","Guava","papaya"]
+for  x in fruits:
+    print(x)
+print(fruits)
+print("Third commit")
+print("diff commit")
+print("One more commit")
+print("last commit")
+
+  
