b4ae3cd8087d873a8416304b3cee076ef7e66905
kaisar672 <68120280+kaisar672@users.noreply.github.com>
2024-02-20 15:20:01 +0530

    Update Hello_World.java

--- Hello_World.java
+++ Hello_World.java
@@ -1,5 +1,6 @@
 class Hello_World{
     public static void main(){
         System.out.println("This is Hello World in java programming language");
+        System.out.println("Java is object oriented programming language");
     }
 }
